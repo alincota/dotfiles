a32f2a786c9d3b4bb0fa2cc03a397d038a2bbcc2
Alin Cota <alincota@users.noreply.github.com>
2019-12-18 23:21:54 +0000

    Added -f option forsymlinks

--- setup.sh
+++ setup.sh
@@ -3,12 +3,11 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Create symlinks
-# TODO: consider using "-f" option to remove existing destination files 
-ln -sv "$DOTFILES_DIR/.bash_aliases" ~/.bash_aliases
-ln -sv "$DOTFILES_DIR/.bash_prompt" ~/.bash_prompt
-ln -sv "$DOTFILES_DIR/.bash_profile" ~/.bash_profile
-ln -sv "$DOTFILES_DIR/.functions" ~/.functions
-ln -sv "$DOTFILES_DIR/.vimrc" ~/.vimrc
+ln -svf "$DOTFILES_DIR/.bash_aliases" ~/.bash_aliases
+ln -svf "$DOTFILES_DIR/.bash_prompt" ~/.bash_prompt
+ln -svf "$DOTFILES_DIR/.bash_profile" ~/.bash_profile
+ln -svf "$DOTFILES_DIR/.functions" ~/.functions
+ln -svf "$DOTFILES_DIR/.vimrc" ~/.vimrc
 
 mkdir -p ~/.vim/backups
 mkdir -p ~/.vim/swaps
